4b008c1368366b88dff4002c0338d36081985c4c
WaldenerLee <waldenerlee@gmail.com>
2020-06-16 18:33:51 +0800

    ...

--- app/src/main/java/com/waldener/musicdog/MainActivity.kt
+++ app/src/main/java/com/waldener/musicdog/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.waldener.musicdog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.waldener.musicdog.data.Search
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_search.setOnClickListener {
+            Thread{
+                Search.search("成龙")
+            }.start()
+        }
     }
 }
---
+++ app/src/main/java/com/waldener/musicdog/auth/ApiKey.kt
@@ -0,0 +1,5 @@
+package com.waldener.musicdog.auth
+
+object ApiKey {
+    const val KEY = "AIzaSyBCXBnSJdhXvC5wrONgRHg3HUdN5dcg0_g"
+}
